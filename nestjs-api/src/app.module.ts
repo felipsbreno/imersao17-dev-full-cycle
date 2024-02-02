13e90bd2d201cbd0713d7faf7698cef023f667c9
felipsbreno <brendo.filipe2050@gmail.com>
2024-02-02 04:33:24 -0300

    create resource for auth and orders

--- nestjs-api/src/app.module.ts
+++ nestjs-api/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+import { OrdersModule } from './orders/orders.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule, OrdersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
